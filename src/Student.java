4968cf7888a3e5d7740c10169459b0418d6acbaa
vuongvantu <vuongvantu2001x@gmail.com>
2021-05-23 18:01:52 +0700

    cmt

---
+++ src/Student.java
@@ -0,0 +1,78 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Student {
+    private String name;
+    private int age;
+    private String address;
+
+    public Student() {
+    }
+
+    public Student(String name, int age, String address) {
+        super();
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Student@name=" + name + ",age=" + age + ",address=" + address;
+    }
+}
+
+class Main {
+
+    public static void main(String[] args) {
+        Student student1 = new Student("Nam",20, "HN");
+        Student student2 = new Student("Hung",21, "HN");
+        Student student3 = new Student("Ha",22, "HN");
+        // write your code here
+        Map<Integer, Student> studentMap = new HashMap<Integer, Student>();
+        studentMap.put(1,student1);
+        studentMap.put(2,student2);
+        studentMap.put(3,student3);
+        studentMap.put(4,student1);
+
+        for(Map.Entry<Integer, Student> student : studentMap.entrySet()){
+            System.out.println(student.toString());
+        }
+        System.out.println("...........Set");
+        Set<Student> students = new HashSet<Student>();
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
+        students.add(student1);
+
+        for(Student student : students){
+            System.out.println(student.toString());
+        }
+    }
+}
